1efc653dd83eb8a36711e8ce45b1f432438dec5a
Zachary Ryan <zacharyryan@zacharys-imac.local>
2015-12-28 05:15:20 -0800

    Initial Commit

---
+++ Tapper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tapper
+//
+//  Created by Zachary Ryan on 12/28/15.
+//  Copyright © 2015 Zachary Ryan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
